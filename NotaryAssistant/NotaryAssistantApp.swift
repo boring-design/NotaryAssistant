babeb6e000f10acbc83a051283425c5925efea19
STRRL <im@strrl.dev>
2024-11-04 21:58:48 -0800

    Initial Commit

---
+++ NotaryAssistant/NotaryAssistantApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotaryAssistantApp.swift
+//  NotaryAssistant
+//
+//  Created by STRRL on 2024-11-04.
+//
+
+import SwiftUI
+
+@main
+struct NotaryAssistantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
